f4f94a9be58d13074ee3ef5c17d95f442ec189de
Henry Arnold <devharnold@gmail.com>
2024-03-19 02:06:38 +0300

    make change

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,25 +4,7 @@ import os
 import asyncio
 import random
 
-async def wait_random(max_delay: int) -> int:
-    max_delay = 10
+async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-
-
-
-
-
-
- #async def wait_n(n, max_delay: int):
- #   delay = max_delay / n
- #   tasks = []
- #   for i in range(n):
- #       tasks.append(asyncio.create_task(asyncio.sleep(delay)))
- #       for task in tasks:
- #           await task
- #           print(delay)
- #   
- #   return tasks
