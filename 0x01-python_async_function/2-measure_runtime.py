1d5a1a57b549077de09c91eb639521faea8707b8
Henry Arnold <devharnold@gmail.com>
2024-03-19 08:15:23 +0300

    configure task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
+
+'''
+Using the asyncio module to generate and execyte tasks
+'''
 import asyncio
 import time
 
+'''Import wait_n from the prev file'''
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
+    '''
+    Function that measures the total execution time for `wait_n`
+    and returns the `total_time / n`
+    Attributes:
+        n: number of times to execute `wait_n`
+        max_delay: maximum delay of each execution of `wait_random`
+    Function should return a float
+    '''
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     return (time.time() - start_time) / n
