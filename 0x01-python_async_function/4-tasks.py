8eef9cefdfd21b49a58890784be7e4af3b33aeb4
Henry Arnold <devharnold@gmail.com>
2024-03-19 08:27:33 +0300

    modify last task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+
+'''
+Using the asyncio module to generate tasks
+'''
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Executes the wait_random and returns the list of all the delays(float)
+    Should be sorted but without using the `sort()` function
+    Attributes:
+        max_delay: the maximum delay time taken
+        wait_times: 
+    '''
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
