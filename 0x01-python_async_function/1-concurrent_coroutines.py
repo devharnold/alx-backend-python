c5d0a0bbf0cb9bcfd39fc11ddce474e7528374e8
Henry Arnold <devharnold@gmail.com>
2024-03-19 08:07:28 +0300

    correct task1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-
+'''
+Using the asyncio to execute tasks
+'''
 import asyncio
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """Executes wait random n times
-    Arguments:
-        n (n): The number of times to execute wait_random.
-        max_delay (int): The maximum delay for each executio of wait_random
-    """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Executes the wait_random and returns the list of all the delays(float values)
+    The list should be in ascending order without using `sort()`
+    Attributes: 
+        n: number of times taken to execute wait_random
+        max_delay: maximum delay of each execution of wait_random
+    '''
+    wait_times = asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+    return sorted(await wait_times)
