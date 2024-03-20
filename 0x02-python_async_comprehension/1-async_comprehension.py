678332301a766bd9b0325894b2d87f4273199c7a
Henry Arnold <devharnold@gmail.com>
2024-03-20 05:52:02 +0300

    mass push

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+using asyncio module with comprehension
+'''
+from typing import List
+from importlib import import_module as using
+
+async_generator = using('0-async_generator').async_generator
+
+
+async def async_cmprehension() -> List[float]:
+    '''
+    generates a list of 10 numbers 
+    returns a listof 10 numbers
+    '''
+    return [num async for num in async_generator()]
