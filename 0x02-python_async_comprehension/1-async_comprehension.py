b0af62cdaa7eee47bf0a95e147c3d9327a29e224
Henry Arnold <devharnold@gmail.com>
2024-03-20 06:00:55 +0300

    make doc

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-'''
-using asyncio module with comprehension
-'''
+"""using asyncio module with comprehension"""
 from typing import List
 
 
@@ -9,8 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_cmprehension() -> List[float]:
-    '''
-    generates a list of 10 numbers 
-    returns a listof 10 numbers
-    '''
+    """generates a list of 10 numbers 
+    returns a listof 10 numbers"""
     return ([_ async for _ in async_generator()])
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-'''
-using asyncio nmodule
-'''
+"""
+using asyncio module
+"""
 
 import asyncio
 import time
@@ -10,10 +10,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''
+    """
     Executes async_comprehension 4 times and measure the
     total execution time
-    '''
+    """
     start_time = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
     return (time.perf_counter() - start_time)
