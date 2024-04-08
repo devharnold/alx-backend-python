3c36827ad4168a86f6b0aacfb784bbc4c7c4f726
Henry Arnold <devharnold@gmail.com>
2024-04-08 12:52:13 +0300

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,3 +14,14 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+class TestAccessNestedMap(unittest.TestCase):
+    """access nested map execution module"""
+    def test_access_nested_map_execution(self, nested_map, path, expected_result):
+        nested_map = {"a": {"b": {"c": 1}}}
+
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, ["a", "x", "z"])
+
+        with self.assertRaises(KeyError):
+            access_nested_map({"a": 1}, ["a", "b", "c"])
