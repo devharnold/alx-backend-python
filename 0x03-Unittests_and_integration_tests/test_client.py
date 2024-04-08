8e34c28213ad6c9e31c1886ea3cd902a5bc3e707
Henry Arnold <devharnold@gmail.com>
2024-04-08 23:59:55 +0300

    test public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test client module"""
 import unittest
 from parameterized import parameterized, parameterized_class
-from unittest.mock import Mock, patch
+from unittest.mock import mock, patch, PropertyMock, patch
 from unittest.mock import MagicMock
 from typing import Dict
 from client import GithubOrgClient
@@ -22,3 +22,11 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(g_org_client.org(), repo)
         mocked_fn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
     
+class TestGithubOrgClient(unittest.TestCase):
+    #@mock.patch('GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos_url(self) -> str:
+        """testing the `_public_repos_url` property"""
+        with patch("GithubClient.org", new_callable=PropertyMock,) as mock_org:
+            mock_org.return_value = {'repos_url': "https://api.github.com/users/google/repos",}
+            self.assertEqual(GithubOrgClient("google")._public_repos_url, "https://api.github.com/users/google/repos",)
+
